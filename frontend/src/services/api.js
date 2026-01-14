0ac417abbe566c87f516089101cede2b7099ad8c
dvdbyte <dvdbyte1@gmail.com>
2026-01-14 12:15:49 +0100

    Fixed cors error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://farmmoniweb.onrender.com/api"
+const API_URL = "https://farmmoni-f4hk.onrender.com/api"
 
 const api = axios.create({
   baseURL: API_URL, 
